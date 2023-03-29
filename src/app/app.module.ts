14c7d55aa618bf284798efad07448475e2adac70
danigelabert <73782173+danigelabert@users.noreply.github.com>
2023-03-29 09:55:33 +0200

    ACABAT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
+import { ExamenComponent } from './examen/examen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExamenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
